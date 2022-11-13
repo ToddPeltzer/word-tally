3c394dd029a8cfbf8dab20e4d6a18db45b0f7a09
toddpeltzer <toddpeltzer14@gmail.com>
2022-11-13 16:54:16 -0700

    using paragraph text now

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,9 +3,7 @@ import './styles/main.css';
 
 function main() {
 
-    // const test = "Todd todd todd ally ally todd! penny seta seta23 hi hey be a i todd todd Todd todd todd ally ally todd! penny seta seta23 hi hey be a i todd todd "
-    const test2 = `They had no proof. He knew that they knew he had done it but they didn't have any proof. It was a huge distinction and it was the difference between him keeping his freedom or being locked away for decades. They continued to question him, probing him for information that they could use against him or find the proof they needed to put him away. He smiled and continued to block their every inquiry by feigning his innocence for a crime they all knew he committed.
-    What were the chances? It would have to be a lot more than 100 to 1. It was likely even more than 1,000 to 1. The more he thought about it, the odds of it happening had to be more than 10,000 to 1 and even 100,000 to 1. People often threw around the chances of something happening as being 1,000,000 to 1 as an exaggeration of an unlikely event, but he could see that they may actually be accurate in this situation. Whatever the odds of it happening, he knew they were big. What he didn't know was whether this happening was lucky or unlucky.`
+    const paragraphText = document.querySelector("p").innerText;
 
     //final outcome will be stored here
     let answerArray = [];
@@ -13,6 +11,9 @@ function main() {
     let letterCount = 2;
     //default font size in rem
     let minimumFontSize = 1;
+    const manyColor = 'red';
+    const fewColor = 'orange';
+    const littleColor = 'green';
 
     function wordCount(e) {
         //finds anything that is not a letter and gets rid of them
@@ -45,21 +46,24 @@ function main() {
     
     }
 
-    wordCount(test2)
+    wordCount(paragraphText)
 
     return (
         <div>
+            <p>
+            They had no proof. He knew that they knew he had done it but they didn't have any proof. It was a huge distinction and it was the difference between him keeping his freedom or being locked away for decades. They continued to question him, probing him for information that they could use against him or find the proof they needed to put him away. He smiled and continued to block their every inquiry by feigning his innocence for a crime they all knew he committed. What were the chances? It would have to be a lot more than 100 to 1. It was likely even more than 1,000 to 1. The more he thought about it, the odds of it happening had to be more than 10,000 to 1 and even 100,000 to 1. People often threw around the chances of something happening as being 1,000,000 to 1 as an exaggeration of an unlikely event, but he could see that they may actually be accurate in this situation. Whatever the odds of it happening, he knew they were big. What he didn't know was whether this happening was lucky or unlucky.
+            </p>
             <ul className='list-container'>
                 {answerArray.map(object => {
                     let newFontSize = object.count+minimumFontSize;
                     let color = "";
                         function fontColor(count) {
                         if (count < 5) {
-                            color = 'green';
+                            color = littleColor;
                         } else if (count < 10) {
-                            color = 'orange';
+                            color = fewColor;
                         } else {
-                            color = 'red';
+                            color = manyColor;
                         }
                         }
                         fontColor(object.count);
