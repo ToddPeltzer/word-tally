8abcaa72d1f4d96cfa3ebd642bddc524ba19b16c
toddpeltzer <toddpeltzer14@gmail.com>
2022-11-14 09:50:04 -0700

    basics working

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,83 +1,23 @@
 import React from 'react';
+import WordCount from './wordCounts';
 import './styles/main.css';
 
 function main() {
 
-    const paragraphText = document.querySelector("p").innerText;
-
-    //final outcome will be stored here
-    let answerArray = [];
-    //minimum length of words being used
-    const letterCount = 2;
-    //default font size in 'rem'
-    const minimumFontSize = 1;
-    const manyColor = 'red';
-    const fewColor = 'orange';
-    const littleColor = 'green';
-
-    function wordCount(e) {
-        //finds anything that is not a letter and gets rid of them
-        let noSymbols = e.toLowerCase().replace(/[^a-z ]/g, "").split(" ");
-        let approvedWords = [];
+    // const element = document.querySelector("p").innerHTML;
+    // console.log(element)
     
-        // filter words to get anything meeting letterCount requirement
-        approvedWords = noSymbols.filter(word => word.length >= letterCount);
-        
-        // creating an object to store the word and count into
-        let wordLookup = {}; 
-        
-        for (let i = 0; i < approvedWords.length; i++) {
-            // if word is not in the object
-            if (!wordLookup[approvedWords[i]]) {
-                // adding the word as the key and then 1 as the initial value
-                wordLookup[approvedWords[i]] = 1;
-                continue    
-            }
-            //if word is existing then add a value
-            wordLookup[approvedWords[i]]++
-        }
-        
-        // destructure and add to answer array
-        for (const [key, value] of Object.entries(wordLookup)) {
-            answerArray.push({word: key, count: value});
-        }
     
-        return answerArray
-    
-    }
-
-    wordCount(paragraphText)
-
     return (
         <div>
-            <p>
+            {/* <p>
             They had no proof. He knew that they knew he had done it but they didn't have any proof. It was a huge distinction and it was the difference between him keeping his freedom or being locked away for decades. They continued to question him, probing him for information that they could use against him or find the proof they needed to put him away. He smiled and continued to block their every inquiry by feigning his innocence for a crime they all knew he committed. What were the chances? It would have to be a lot more than 100 to 1. It was likely even more than 1,000 to 1. The more he thought about it, the odds of it happening had to be more than 10,000 to 1 and even 100,000 to 1. People often threw around the chances of something happening as being 1,000,000 to 1 as an exaggeration of an unlikely event, but he could see that they may actually be accurate in this situation. Whatever the odds of it happening, he knew they were big. What he didn't know was whether this happening was lucky or unlucky.
-            </p>
-            <ul className='list-container'>
-                {answerArray.map(object => {
-                    let newFontSize = object.count+minimumFontSize;
-                    let color = "";
-                        function fontColor(count) {
-                        if (count < 5) {
-                            color = littleColor;
-                        } else if (count < 10) {
-                            color = fewColor;
-                        } else {
-                            color = manyColor;
-                        }
-                        }
-                        fontColor(object.count);
-                        const fontStyle = {
-                        fontSize: `${newFontSize}rem`,
-                        color: color
-                        }
-                    return (
-                        <li className="word-container" style={fontStyle}>
-                            {object.word}
-                        </li>
-                    )
-                })}
-            </ul>
+            </p> */}
+            <div>
+                <WordCount 
+                    // paragraphText = {element}
+                />
+            </div>
         </div>
     );
 }
---
+++ src/components/wordCounts.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import './styles/wordCount.css';
+
+function main() {
+
+    let paragraphText = "They had no proof. He knew that they knew he had done it but they didn't have any proof. It was a huge distinction and it was the difference between him keeping his freedom or being locked away for decades. They continued to question him, probing him for information that they could use against him or find the proof they needed to put him away. He smiled and continued to block their every inquiry by feigning his innocence for a crime they all knew he committed. What were the chances? It would have to be a lot more than 100 to 1. It was likely even more than 1,000 to 1. The more he thought about it, the odds of it happening had to be more than 10,000 to 1 and even 100,000 to 1. People often threw around the chances of something happening as being 1,000,000 to 1 as an exaggeration of an unlikely event, but he could see that they may actually be accurate in this situation. Whatever the odds of it happening, he knew they were big. What he didn't know was whether this happening was lucky or unlucky.";
+    //final outcome will be stored here
+    let answerArray = [];
+    //minimum length of words being used
+    const letterCount = 2;
+    //default font size in 'rem'
+    const minimumFontSize = 1;
+    const manyColor = 'red';
+    const fewColor = 'orange';
+    const littleColor = 'green';
+
+    function wordCount(e) {
+
+        //finds anything that is not a letter and gets rid of them
+        let noSymbols = e.toLowerCase().replace(/[^a-z ]/g, "").split(" ");
+        let approvedWords = [];
+    
+        // filter words to get anything meeting letterCount requirement
+        approvedWords = noSymbols.filter(word => word.length >= letterCount);
+        
+        // creating an object to store the word and count into
+        let wordLookup = {}; 
+        
+        for (let i = 0; i < approvedWords.length; i++) {
+            // if word is not in the object
+            if (!wordLookup[approvedWords[i]]) {
+                // adding the word as the key and then 1 as the initial value
+                wordLookup[approvedWords[i]] = 1;
+                continue    
+            }
+            //if word is existing then add a value
+            wordLookup[approvedWords[i]]++
+        }
+        
+        // destructure and add to answer array
+        for (const [key, value] of Object.entries(wordLookup)) {
+            answerArray.push({word: key, count: value});
+        }
+    
+        return answerArray
+    
+    }
+
+    wordCount(paragraphText)
+
+    return (
+        <div>
+            <p>{paragraphText}</p>
+            <ul className='list-container'>
+                {answerArray.map(object => {
+                    let newFontSize = object.count+minimumFontSize;
+                    let color = "";
+                        function fontColor(count) {
+                        if (count < 5) {
+                            color = littleColor;
+                        } else if (count < 10) {
+                            color = fewColor;
+                        } else {
+                            color = manyColor;
+                        }
+                        }
+                        fontColor(object.count);
+                        const fontStyle = {
+                        fontSize: `${newFontSize}rem`,
+                        color: color
+                        }
+                    return (
+                        <li className="word-container" style={fontStyle}>
+                            {object.word}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default main;
